560b294a5b9c3b22c74e63fd9be79d95ebf043ba
Camillus Anko <ankocamillus@gmail.com>
2025-07-30 18:12:25 +0000

    Day 23: Filtered Forecast Data

--- backend.py
+++ backend.py
@@ -9,11 +9,22 @@ def get_data(place, forecast_days=None, kind=None):
 
     data = response.json()
 
-    return data
+    filtered_data = data["list"]
+    #Number of values i.e nr_values
+    nr_values = 8 * forecast_days
+    filtered_data = filtered_data[:nr_values]
+
+    if kind == "Temperature":
+        filtered_data = [dict["main"]["temp"] for dict in filtered_data] #Gives a list of all 16 temperatures
+
+    if kind == "Sky":
+        filtered_data = [dict["weather"][0]["main"] for dict in filtered_data]
+        
+    return filtered_data
 
 
 
 
 
 if __name__ == "__main__":
-    print(get_data(place="Tokyo"))
+    print(get_data(place="Tokyo", forecast_days=3, kind="Temperature"))
